2660075eee79382cc242afee5dc8143d62c2f5de
Filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2023-12-20 17:26:56 -0300

    ajustes com samu

---
+++ Shared/ViewModels/PaginacaoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IBGE_Blazor.Shared.ViewModels
+{
+    public class PaginacaoViewModel
+    {
+
+        public string Id { get; set; }
+
+     public string State { get; set; }
+
+   public string City { get; set; }
+    }
+}
