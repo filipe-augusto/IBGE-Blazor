85110d0706ae7a8213849c3fbda7c8dd5410eac0
Filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2023-12-21 00:12:06 -0300

    autenticação e outros ajustes no geral

--- Models/Localidade.cs
+++ Models/Localidade.cs
@@ -5,18 +5,18 @@ namespace IBGE_Blazor.Models
     public class Localidade
     {
         [Required(ErrorMessage = "Codigo da cidade é necessario")] 
-        [MaxLength(7, ErrorMessage = "Digite 7 caracateres para o codigo da cidade")]
-        [MinLength(7, ErrorMessage = "Digite 7 caracateres para o codigo da cidade")]
+        [MaxLength(7, ErrorMessage = "Digite 7 caracteres para o codigo da cidade")]
+        [MinLength(7, ErrorMessage = "Digite 7 caracteres para o codigo da cidade")]
         public string Id { get; set; }
 
         [Required(ErrorMessage = "Estado é necessario")]
-        [MaxLength(2, ErrorMessage = "Digite no maximo 2 caracateres para o estado")]
-        [MinLength(2, ErrorMessage = "Digite no minimo 2 caracateres para o estado")]
+        [MaxLength(2, ErrorMessage = "Digite no maximo 2 caracteres para o estado")]
+        [MinLength(2, ErrorMessage = "Digite no minimo 2 caracteres para o estado")]
         public string State { get; set; }
 
 
         [Required(ErrorMessage = "Cidade é necessario")]
-        [MinLength(2, ErrorMessage = "Digite no minimo 2 caracateres para a cidade")]
+        [MinLength(2, ErrorMessage = "Digite no minimo 2 caracteres para a cidade")]
         public string City { get; set; }
 
     }
